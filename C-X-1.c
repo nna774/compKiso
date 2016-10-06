ee7f40917b937c5b80239bd189002baacd36d515
NoNameA 774 <nonamea774@gmail.com>
2016-10-06 21:42:22 +0900

    それっぽいのが実装できた。

--- C-X-1.c
+++ C-X-1.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
+#include <math.h>
 
 double const eps = 1.0e-5;
+int const MAX_STEP = 100000;
+int const DIV_D = 1000;
+
+void calc_powers(double const * const spring_constants,
+		 double const * const naturals,
+		 double const * const xs,
+		 double const * const ys,
+		 double const * const current,
+		 double powers[][2]) {
+  int i = 0;
+  for(i = 0; i < 3; ++i) {
+    double len = sqrt(pow(xs[i] - current[0], 2) + pow(ys[i] - current[1], 2));
+//    powers[i] = len < naturals[i] ? 0 : (spring_constants[i] * (len - naturals[i]));
+    powers[i][0] = len < naturals[i] ? 0 : (spring_constants[i] * (xs[i] - current[0]));
+    powers[i][1] = len < naturals[i] ? 0 : (spring_constants[i] * (ys[i] - current[1]));
+  }
+}
 
 int main() {
   double spring_constants[3];
   double naturals[3];
   double xs[3], ys[3];
-  int i;
+  int i, t = 0;
+  double current[2] = { 100, 100 }; // x, y
+  double prev[2] = { 0, 0 }; // x, y
   for(i = 0; i < 3; ++i) {
     printf("ばね%d:\n", i+1);
     printf("ばね定数?:\n");
@@ -29,4 +49,28 @@ int main() {
     printf("Y座標: ");
     printf("%lf\n",  ys[i]);
   }
+  for(t = 0; t < MAX_STEP; ++t) {
+    double powers[3][2]; // 各ばねについて{x, y}方向
+    double sums[2] = { 0, 0 };
+    calc_powers(spring_constants, naturals, xs, ys, current, powers);
+    for(i = 0; i < 3; ++i) {
+//      printf("%lf, %lf\n", powers[i][0], powers[i][1]);
+      sums[0] += powers[i][0];
+      sums[1] += powers[i][1];
+    }
+    // ひっぱられる時に動く微少量
+    double const dx = sums[0] / DIV_D;
+    double const dy = sums[1] / DIV_D;
+    current[0] += dx;
+    current[1] += dy;
+//    puts("");
+    printf("%lf, %lf\n", current[0], current[1]);
+
+    if (sqrt(pow(prev[0] - current[0], 2) + pow(prev[1] - current[1], 2)) < eps) {
+      break;
+    }
+    prev[0] = current[0];
+    prev[1] = current[1];
+  }
+  printf("%lf, %lf\n", current[0], current[1]);
 }
